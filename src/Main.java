8a39c5502e1355760963013b90435af3fa0b30f3
eniciodehaas <43574607+eniciodehaas@users.noreply.github.com>
2018-11-14 12:28:36 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        double geld = 20;
+
+        //vragen
+        String[] vendingVragen = {"Wil je iets eten of drinken?", "Warm of koud?", "Zoet of Hartig?", "Gezond of Ongezond?", "met of zonder alcohol?"};
+
+        //antwoorden
+        String[] vendingAntwoorden = new String[15];
+
+
+    }
+
+}
