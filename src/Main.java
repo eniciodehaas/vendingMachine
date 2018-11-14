cb03dfb2c404b62db50ad15654be82d9de431e4b
eniciodehaas <43574607+eniciodehaas@users.noreply.github.com>
2018-11-14 12:32:51 +0100

    vragen voor "eten"

--- src/Main.java
+++ src/Main.java
@@ -14,13 +14,26 @@ public class Main {
         //antwoorden
         String[] vendingAntwoorden = new String[15];
         while (geld >= 0.85){
-            
+
             System.out.println("Je hebt nog €" +geld+",-");
 
 
             //eten of drinken
             System.out.println(vendingVragen[0]);
             vendingAntwoorden[0] = scan.next();
+
+            //als het eten is
+            if (vendingAntwoorden[0].equals("eten")){
+
+
+                System.out.println(vendingVragen[1]);
+                vendingAntwoorden[1] = scan.next();
+
+                System.out.println(vendingVragen[2]);
+                vendingAntwoorden[2] = scan.next();
+
+                System.out.println(vendingVragen[3]);
+                vendingAntwoorden[3] = scan.next();
         }
 
     }
